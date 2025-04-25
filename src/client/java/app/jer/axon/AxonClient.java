d5446b3a05bfbe605d886f108fbcf4ee15229169
Jeremy <jeremy@jeremywoolley.com>
2025-04-24 23:29:31 -0700

    Initial template

---
+++ src/client/java/app/jer/axon/AxonClient.java
@@ -0,0 +1,10 @@
+package app.jer.axon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AxonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
